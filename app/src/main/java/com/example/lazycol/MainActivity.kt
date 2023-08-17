d02dceef106915fa479e2b1021c74b89109e3815
mayank <mayankrajput40558@gmail.com>
2023-08-17 15:19:03 +0530

    feat: added clickable response on image which opens url

--- app/src/main/java/com/example/lazycol/MainActivity.kt
+++ app/src/main/java/com/example/lazycol/MainActivity.kt
@@ -1,22 +1,40 @@
 package com.example.lazycol
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -63,7 +81,7 @@ class MainActivity : ComponentActivity() {
                 setContent {
                     LazyColumn {
                         items(newsArray) {
-                           NewsItem(imageUrl = it.imageUrl, title = it.title, author = it.author)
+                           NewsItem(imageUrl = it.imageUrl, title = it.title, author = it.author,it.url)
                         }
                     }
                 }
@@ -74,14 +92,27 @@ class MainActivity : ComponentActivity() {
         MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
     }
     @Composable
-    fun NewsItem(imageUrl: String, title: String, author: String) {
+    fun NewsItem(imageUrl: String, title: String, author: String,url: String) {
         Column {
-            Image(
-            painter = rememberImagePainter(imageUrl),
-            contentDescription = null,
-            modifier = Modifier
-                .size(200.dp)
-        )
+            val imageModifier = Modifier
+                .size(width = 450.dp, height = 250.dp)
+                .border(BorderStroke(1.dp, Color.Black))
+                .padding(16.dp)
+                .clip(RoundedCornerShape(12.dp))
+                .clickable {
+                    // Open the URI when the image is clicked.
+                    startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
+                }
+            Box(
+                contentAlignment = Alignment.Center
+            ){
+                Image(
+                    painter = rememberImagePainter(imageUrl),
+                    contentDescription = null,
+                    modifier = imageModifier,
+                    contentScale = ContentScale.Crop
+                )
+            }
             Text(
                 title,
                 style = MaterialTheme.typography.titleMedium,
